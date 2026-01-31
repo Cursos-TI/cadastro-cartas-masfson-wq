b77b52969e9daef6f3c17849615194f82d203bda
Marcão <masfson@gmail.com>
2026-01-31 18:32:23 +0000

    Acrescentei um novo comentario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,34 +22,36 @@
 
     // Entrada da carta 1
 
-    printf("Cadastro da carta 1: \n");
+    printf("Cadastro da carta 1: ");
     scanf("%d", &estado);
     printf("Estado:\n");
 
-    printf("Código da carta:\n");
+    printf("Código da carta: ");
     scanf("%d", &código1);
     printf("O codigo é: %f\n, codigo");
 
-    printf("Nome da cidade:\n");
+    printf("Nome da cidade: ");
     scanf("%d", &cidade);
     printf("A cidade é: %d, idade")
 
-    printf("População: \n");
+    printf("População: ");
     scanf("%d", &população1);
     printf("A população é: %f\n, populacao")
 
-    printf("Area em Km²:\n");
+    printf("Area em Km²: ");
     scanf("%f", &area 1);
     printf("A área é: %f\n area");
 
-
-    printf("PIB", \n);
+    printf("PIB: ");
     scanf("%f", &pib 1);
     printf("A PIB é: %f")
 
-    printf("Número de pontos turisticos: \n");
+    printf("Número de pontos turisticos: ");
     scanf("%d", &pontos 1);
     ´printf("O número de pontos turisticos são: %f");
+
+    // Cadastro da carta
+    printf(" Cadastro da carta 2");
     
     
 
